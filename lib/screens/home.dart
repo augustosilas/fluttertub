09026089b6f64256d6311a262415da6ffe778b21
Silas Augusto <augusto.fernandes775@gmail.com>
2020-12-09 13:04:43 -0300

    creating main screen layout
    Signed-off-by: Silas Augusto <augusto.fernandes775@gmail.com>

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,6 +3,30 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 50.0,
+          child: Image.asset("images/yl.jpg"),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black87,
+        actions: [
+          Align(
+            alignment: Alignment.center,
+            child: Text("0"),
+          ),
+          IconButton(
+            icon: Icon(Icons.star),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
