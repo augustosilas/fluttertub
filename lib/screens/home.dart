085dac33dee34f426c86a77d6b7324acf29c43bd
Silas Augusto <augusto.fernandes775@gmail.com>
2020-12-09 13:54:53 -0300

    finalizing search system
    Signed-off-by: Silas Augusto <augusto.fernandes775@gmail.com>

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -23,8 +23,10 @@ class Home extends StatelessWidget {
           ),
           IconButton(
             icon: Icon(Icons.search),
-            onPressed: () {
-              showSearch(context: context, delegate: DataSearch());
+            onPressed: () async {
+              String result =
+                  await showSearch(context: context, delegate: DataSearch());
+              print(result);
             },
           )
         ],
